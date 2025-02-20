f3540f0d8465d1941f785165905812065abef7a1
Andrew McCleary <theandrewmccleary@gmail.com>
2025-02-19 22:52:51 -0500

    baseline protocol class created

--- Networking/HACProtocol.java
+++ Networking/HACProtocol.java
@@ -2,11 +2,26 @@ package Networking;
 
 import java.net.*;
 
-public class HACProtocol {
-    String version;
+public class HACProtocol { //what class format? javadoc needed? tostring etc needed?
+    //protocol specific fields
+    String version; //protocol version (do we need this?)
+    int length; //length of data
+    int numNodes; //number of nodes on network
+    boolean nodesUp []; //array of booleans representing status of all other nodes
+    String localFiles []; //array of file names on node
+
+    //existing packet format
     DatagramPacket packet;
 
-    public HACProtocol (String data, int length, InetAddress address, int port, String version) {
+    public HACProtocol (byte[] data, InetAddress address, int port, String version, int length, int numNodes, String[] localFiles) {
+        //create and store packet
+        this.packet = new DatagramPacket(data, data.length, address, port);
 
+        //initialize protocol variables
+        this.version = version;
+        this.length = length;
+        this.numNodes = numNodes; //how do i get this? is constructor ideal?
+        this.nodesUp = new boolean[numNodes];
+        this.localFiles = localFiles;
     }
 }
