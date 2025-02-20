70d3e4ddc0e96e00c1312fd679720052610abe94
Andrew McCleary <theandrewmccleary@gmail.com>
2025-02-20 13:08:52 -0500

    updated protocol somewhat, made it serializable (0% implemented tho), added some home files to fuck around with

--- Networking/HACProtocol.java
+++ Networking/HACProtocol.java
@@ -1,19 +1,21 @@
 package Networking;
 
+import java.io.Serializable;
 import java.net.*;
 
-public class HACProtocol { //what class format? javadoc needed? tostring etc needed?
-    //protocol specific fields
+public class HACProtocol implements Serializable { //what class format? javadoc needed? tostring etc needed?
+    //protocol control fields
     String version; //protocol version (do we need this?)
+    int sequenceNumber; //sequence of packet sent
     int length; //length of data
     int numNodes; //number of nodes on network
     boolean nodesUp []; //array of booleans representing status of all other nodes
     String localFiles []; //array of file names on node
 
-    //existing packet format
+    //protocol packet data
     DatagramPacket packet;
 
-    public HACProtocol (byte[] data, InetAddress address, int port, String version, int length, int numNodes, String[] localFiles) {
+    public HACProtocol (byte[] data, InetAddress address, int port, String version, int sequenceNumber, int length, int numNodes, String[] localFiles) {
         //create and store packet
         this.packet = new DatagramPacket(data, data.length, address, port);
 
