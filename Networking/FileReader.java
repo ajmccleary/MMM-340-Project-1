c749b5acb646ff558cf14dba44fc436da211e2b5
BradyG318 <160978995+BradyG318@users.noreply.github.com>
2025-02-22 23:08:01 -0500

    FileReader outputs the files in an arraylist of all the files
    My commit message was too long but whatever you get the idea

--- Networking/FileReader.java
+++ Networking/FileReader.java
@@ -1,22 +1,40 @@
 package Networking;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class FileReader {
-    ArrayList<Path> outList;
-
-    public FileReader(String filePath) {
-        try(Stream<Path> paths = Files.walk(Paths.get("Home"))) {
-            paths
-                .filter(Files::isRegularFile)
-                .forEach(System.out::println);
-        } catch(IOException e) {
-            System.out.println("DEBUG: Fuck Off");
+    static ArrayList<Path> outList;
+    static ArrayList<File> outFiles;
+        // paths;
+    
+        public static ArrayList<File> FileReader() {
+            outList = new ArrayList<>();
+            outFiles = new ArrayList<>();
+            try(Stream<Path> paths = Files.walk(Paths.get("Home"))) {
+                outList=paths
+                    .filter(Files::isRegularFile)
+                    .collect(Collectors.toCollection(ArrayList::new));
+            } catch(IOException e) {
+                System.out.println("DEBUG: Fuck Off I'm going to kill a man");
+            }
+            for(int i = 0; i<outList.size(); i++) {
+                outFiles.add(i, outList.get(i).toFile());
+            }
+            
+            return outFiles; 
         }
-    }
+        public List<Path> getFilePaths() {
+            return outList;
+        }
+        public static void main(String[] args) {
+            System.out.println(FileReader().toString());
+        }
 }
