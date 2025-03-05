d06b811e5c2c810e80bebb2c242d1118135cd465
asHasHasHas <168456398+asHasHasHas@users.noreply.github.com>
2025-03-05 15:08:10 -0500

    i lied here are the moved files

---
+++ Networking/Node.java
@@ -0,0 +1,62 @@
+package Networking;
+
+import java.io.File;
+import java.util.ArrayList;
+
+/**
+ * This is a node object for storing information on a connected Node/machine
+ * @param IPAddress used to store the IP of the connected machine
+ * @param containedFiles contains a list of all files on the Node
+ * @param portNum int value that stores the port of the connected device
+ */
+public class Node {
+    private String IPAddress;
+    private ArrayList<File> containedFiles;
+    private boolean hasTimedOut, isSelf;
+    private int secSinceHeartbeat;
+    private int portNum;
+    
+    public Node(String IPAddress, int portNum, ArrayList<File> containedFiles) {
+        this.IPAddress = IPAddress;
+        this.portNum = portNum;
+        this.hasTimedOut = false;
+        this.IPAddress = IPAddress;
+        this.containedFiles = containedFiles;
+        this.isSelf = false;
+    }
+    
+    /**
+     * Checks if the node is still online
+     * @return true IF node is down
+     */
+    public boolean timeOut() {
+        if(secSinceHeartbeat > 30) {
+            hasTimedOut = true;
+        }
+        return hasTimedOut; 
+    }
+    public String getFileNames() {
+        String directoryList = "";
+        for(File file:containedFiles) {
+            directoryList += "\t" + file.getName() + "\n";
+        }
+        return directoryList;
+    }
+
+    public Boolean isNodeSelf() {
+        return this.isSelf;
+    }
+    
+
+    public void heartbeatRecieved() { secSinceHeartbeat = 0; }
+    public void heartbeat() { secSinceHeartbeat++;}
+    public void setFiles(ArrayList<File> files) {this.containedFiles = files;}
+
+    public void setToSelf() {this.isSelf = true;}
+    public void unSetToSelf() {this.isSelf = false;}
+    public String getID() {return (this.IPAddress + ":" + portNum);}
+    public String getIP() { return this.IPAddress; }
+    public int getPort() { return this.portNum; }
+    public ArrayList<File> getFileList() { return containedFiles; }
+    public int getLastHrtBt() { return this.secSinceHeartbeat; }
+}
